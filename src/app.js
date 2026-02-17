3a979a7bd973a7a112c4f3d2c6068d5fe3170b92
maru <teawoo913@gmail.com>
2026-02-17 15:01:21 -0800

    demo07 update

--- src/app.js
+++ src/app.js
@@ -15,3 +15,34 @@ function sayHello() {
   // TODO: implement your logic here
 }
 document.addEventListener('DOMContentLoaded', sayHello);
+
+import {
+    onAuthReady
+} from "./authentication.js"
+
+function showName() {
+      const nameElement = document.getElementById("name-goes-here"); // the <h1> element to display "Hello, {name}"
+      if (!nameElement) return;
+      // Wait for Firebase to determine the current authentication state.
+      // onAuthReady() runs the callback once Firebase finishes checking the signed-in user.
+      // The user's name is extracted from the Firebase Authentication object
+      // You can "go to console" to check out current users. 
+      onAuthReady((user) => {
+          if (!user) {
+              // If no user is signed in → redirect back to login page.
+              location.href = "/index.html";
+              return;
+          }
+
+          // If a user is logged in:
+          // Use their display name if available, otherwise show their email.
+          const name = user.displayName || user.email;
+
+          // Update the welcome message with their name/email.
+          if (nameElement) {
+              nameElement.textContent = `${name}!`;
+          }
+      });
+}
+
+showName();
