835b0a9dfd1302d9cf74424f72fa3536f865e290
maru <teawoo913@gmail.com>
2026-03-03 15:25:38 -0800

    firestore updates up to screenshot #2

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,8 +9,10 @@
 // ---------------------------------------------------------
 
 // Import Firebase SDK modules (using Firebase v9 modular syntax)
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 
 // ---------------------------------------------------------
 // Read Firebase configuration from Vite environment variables.
@@ -42,3 +44,4 @@ const app = initializeApp(firebaseConfig);
 // or signout operations (that's why we export it).
 // ---------------------------------------------------------
 export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { db } from "./firebaseConfig.js";
+import { doc, onSnapshot } from "firebase/firestore";
+
+// Function to read the quote of the day from Firestore
+function readQuote(day) {
+    const quoteDocRef = doc(db, "quotes", day); // Get a reference to the document
+
+    onSnapshot(quoteDocRef, docSnap => { // Listen for real-time updates
+        if (docSnap.exists()) {          //Document existence check
+            document.getElementById("quote-goes-here").innerHTML = docSnap.data().quote;
+        } else {
+            console.log("No such document!");
+        }
+    }, (error) => {                      //Listener/system error
+        console.error("Error listening to document: ", error);
+    });
+}
+
+readQuote("tuesday");
